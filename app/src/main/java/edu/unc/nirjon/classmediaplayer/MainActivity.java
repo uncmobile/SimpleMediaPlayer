33c8845b7e2027b15cac31f1c5091f43d8b227bd
Shiwei Fang <shiwei@cs.unc.edu>
2016-03-04 10:34:46 -0500

    Initial commit

---
+++ app/src/main/java/edu/unc/nirjon/classmediaplayer/MainActivity.java
@@ -0,0 +1,63 @@
+package edu.unc.nirjon.classmediaplayer;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener{
+
+    MediaPlayer mp = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pauseSong(View v) {
+        Log.v("MP", "pause button clicked");
+        if(mp != null) {
+            mp.pause();
+        }
+    }
+
+    public void playSong(View v) {
+
+        Log.v("MP", "Play button clicked");
+        String str = "http://wwwx.cs.unc.edu/~nirjon/test.mp3";
+        try {
+            if(mp != null) {
+                mp.release();
+                mp = null;
+            }
+
+            mp = new MediaPlayer();
+            mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mp.setDataSource(str);
+            mp.setOnPreparedListener(this);
+            mp.prepareAsync();
+        }catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    protected  void onStop() {
+        super.onStop();
+        if(mp != null) {
+            mp.release();
+            mp = null;
+        }
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        Log.v("MP", "onPrepared called");
+        if (mp != null) {
+            mp.start();
+        }
+    }
+}
